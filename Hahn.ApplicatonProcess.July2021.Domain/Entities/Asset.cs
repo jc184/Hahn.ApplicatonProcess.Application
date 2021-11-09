3141a1aad086ccb7fef15fc5629b35bec9b5b1b8
james chalmers <james.chalmers184@gmail.com>
2021-11-09 15:41:27 +0000

    Add project files.

---
+++ Hahn.ApplicatonProcess.July2021.Domain/Entities/Asset.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hahn.ApplicatonProcess.July2021.Domain.Entities
+{
+    public class Asset
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Symbol { get; set; }  
+    }
+}
